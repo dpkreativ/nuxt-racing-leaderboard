af41a6cc247afa16d2356daca271487f47c2637c
Divine Orji <dpkreativ@gmail.com>
2023-03-27 10:48:33 +0100

    feat: add nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-
-})
+  runtimeConfig: {
+    public: {
+      database: process.env.APPWRITE_DATABASE_ID,
+      collection: process.env.APPWRITE_COLLECTION_ID,
+    },
+  },
+  modules: ['nuxt-appwrite'],
+  appwrite: {
+    endpoint: process.env.APPWRITE_ENDPOINT,
+    project: process.env.APPWRITE_PROJECT_ID,
+  },
+});

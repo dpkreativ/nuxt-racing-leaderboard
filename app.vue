f401b9940bfa04e50b2681719435cda8caaf59e5
Divine Orji <dpkreativ@gmail.com>
2023-03-27 08:40:46 +0100

    feat: set up config, create session, get data

--- app.vue
+++ app.vue
@@ -7,6 +7,21 @@
 <script setup>
 import '@appwrite.io/pink';
 import '@appwrite.io/pink-icons';
+
+const config = useRuntimeConfig();
+
+const { account, client, database } = useAppwrite();
+
+// Create an anonymous session if it doesn't already exist
+if (account.getSession === null) {
+  await account.createAnonymousSession();
+}
+
+// Get initial data from the database
+const { documents } = await database.listDocuments(
+  config.database,
+  config.collection
+);
 </script>
 
 <style>
